1e3b606fd64e7f58245a4ecdc9c8f1ae0f6911c1
Vahidone <v.ban.roma@gmail.com>
2023-10-12 17:58:09 +0200

    add: add store js file

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <script>
+import {store} from "./data/store";
 import CharacterCard from "./components/CharacterCard.vue";
 import CardsContainer from "./components/CardsContainer.vue";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
   name: "App",
+  data () {
+    return {
+      store
+    }
+  },
+  
+ 
   components: {
     CardsContainer,
     CharacterCard,
+    Header,
+    Main,
+    
+    
+   
   },
 };
 </script>
 
 <template>
-  <h1>sono App</h1>
+  <Header />
+  <Main />
+  <CardsContainer />
+  <CharacterCard />
 </template>
 
 <style lang="scss">
-@use "./scss/main.scss";
-@import "bootstrap/scss/bootstrap";
+@use "./components/scss/main.scss";
+
 </style>
