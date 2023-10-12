12d1a88f5593451f3c15e398ba76324a8867557d
Vahidone <v.ban.roma@gmail.com>
2023-10-12 15:33:43 +0200

    feat: basic structure of components

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "CardsContainer",
+};
+</script>
+
+<template></template>
+
+<style></style>
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "CharacterCard",
+};
+</script>
+
+<template></template>
+
+<style></style>
